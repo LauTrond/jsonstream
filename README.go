ba3c716c465c88174ce53e8ad86277ec41b4dd1b
liuchang <liuchang@yimian.com.cn>
2019-07-22 15:51:05 +0800

    README

---
+++ README.go
@@ -0,0 +1,15 @@
+# jsonstream
+
+package jsonstream 提供简单的JSON流式处理。
+
+所谓流式处理，就是可以在不把整个JSON数据加载到内存的前提下完成遍历。
+
+package jsonstream是package json的封装。
+
+## 安装
+
+	go get github.com/LauTrond/jsonstream
+
+## 示例
+
+参见 jsonstream_test.go
